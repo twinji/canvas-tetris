06f0e9419c281058025e6607d0a0efe20e3e3e37
Vishnu Gunapathi <38277626+Twinji@users.noreply.github.com>
2020-01-19 11:50:32 +1030

    Add basic flow

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var pieces = [
         [1, 1, 1, 1]
     ],
     [
-        [1, 1]
+        [1, 1],
         [1, 1]
     ],
     [
@@ -31,7 +31,7 @@ var pieces = [
         [1, 1, 0],
         [0, 1, 1]
     ]
-]
+];
 
 // game variables
 var currentPiece;
@@ -40,7 +40,17 @@ var nextPiece;
 
 // update stuff
 var frameNumber = 0;
-var framesUntilUpdate = 60;
+var framesUntilUpdate = 10;
+
+// keyboard controls
+var up = 38, 
+    down = 40, 
+    right = 39, 
+    left = 37;
+
+var key = new Array();
+document.addEventListener("keydown", function(e) {key[e.keyCode] = true;});
+document.addEventListener("keyup", function(e) {key[e.keyCode] = false;});
 
 // point data type
 var Vector2 = function(x, y) {
@@ -80,19 +90,24 @@ function init(c) {
         grid[i] = new Array(gridColumns);
     }
 
+    // initialize game variables
+    piecePosition = new Vector2(0, 0);
+
 }
 
 function update() {
 
+    console.log(grid);
+
     frameNumber++;
 
     // set next piece if required
-    if (!nextPiece) {
+    if (nextPiece == null) {
         nextPiece = pieces[Math.round(Math.random() * (pieces.length - 1))];
     }
 
     // set current piece if required
-    if (!currentPiece) {
+    if (currentPiece == null) {
         currentPiece = nextPiece;
         nextPiece = null;
         piecePosition = new Vector2(Math.round(Math.random() * (grid[0].length - 1 - currentPiece[0].length)), 0);
@@ -104,31 +119,60 @@ function update() {
         // move logic
         piecePosition.y++;
         
-        if (piecePosition.y + currentPiece.length > gridRows) {
-            
+        // flag on whether to place piece
+        var placeOnGrid = false;
+
+        // check bottom of grid
+        if (piecePosition.y + currentPiece.length > grid.length) {
+            placeOnGrid = true;
         }
 
+        // place piece on grid if required
+        if (placeOnGrid) {
+            piecePosition.y--;
+            for (var i = 0; i < currentPiece.length; i++) {
+                for (var j = 0; j < currentPiece[i].length; j++) {
+                    if (currentPiece[i][j] == 1) {
+                        grid[piecePosition.y + i][piecePosition.x + j] = 1;
+                    }
+                }
+            }
+            currentPiece = null;
+        }
     }
-
 }
 
 function render(c) {
 
     c.clearRect(0, 0, WIDTH, HEIGHT);
+    
+    // draw placed pieces
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid[i].length; j++) {
+            drawBlock(c, j, i, grid[i][j] == 1 ? "grey" : "black");
+        }
+    }
+
+    // draw current piece
+    if (currentPiece != null) {
+        for (var i = 0; i < currentPiece.length; i++) {
+            for (var j = 0; j < currentPiece[i].length; j++) {
+                if (currentPiece[i][j] == 1) {
+                    drawBlock(c, piecePosition.x + j, piecePosition.y + i, rgb(200, 200, 200));
+                }
+            }
+        }
+    }
+    // drawBlock(c, piecePosition.x, piecePosition.y, rgb(200, 200, 200));
 
     // draw grid
     c.strokeStyle = rgb(16, 16, 16);
     for (var i = 0; i <= grid.length; i++) {
         drawLine(c, 0, i * gridCellSize, grid[0].length * gridCellSize, i * gridCellSize, 1);
     }
-    for (var j = 0; j <= grid[0].length; j++) {
-        drawLine(c, j * gridCellSize, 0, j * gridCellSize, grid.length * gridCellSize, 1);
+    for (var i = 0; i <= grid[0].length; i++) {
+        drawLine(c, i * gridCellSize, 0, i * gridCellSize, grid.length * gridCellSize, 1);
     }
-
-    // draw current piece
-    c.fillStyle = rgb(200, 200, 200);
-    
-
 }
 
 // helper for drawing lines
@@ -141,11 +185,14 @@ function drawLine(c, x1, y1, x2, y2, width) {
     c.closePath();
 }
 
-function drawBlock(c, x, y) {
+function drawBlock(c, x, y, color) {
+    var previousColor = c.fillStyle;
+    c.fillStyle = color;
     c.fillRect(x * gridCellSize, y * gridCellSize, gridCellSize, gridCellSize);
+    c.fillStyle = previousColor;
 }
 
 // function for generating rgba() string
 function rgb(r, g, b) {
     return "rgba(" + [r, g, b].join(",") + ",1)";
-  }
+}
